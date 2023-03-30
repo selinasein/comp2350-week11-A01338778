df2e21e2243d8b39b2787406825cca9c9c7f401f
selinasein <neulmisscj@gmail.com>
2023-03-29 18:15:24 -0700

    second commit

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,15 +1,14 @@
-const is_heroku = process.env.IS_HEROKU || false;
+// const is_heroku = process.env.IS_HEROKU || false;
 
-const dbConfigHeroku = "mysql://b1ab7fb2ee03bc:2a484a2d@us-cdbr-east-03.cleardb.com/heroku_3d208ad4bd6f421";
+// const dbConfigHeroku = "mysql://b1ab7fb2ee03bc:2a484a2d@us-cdbr-east-03.cleardb.com/heroku_3d208ad4bd6f421";
 
-const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
+// const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
 
-if (is_heroku) {
-	var databaseConnectionString = dbConfigHeroku;
-}
-else {
-	var databaseConnectionString = dbConfigLocal;
-}
+// if (is_heroku) {
+// 	var databaseConnectionString = dbConfigHeroku;
+// }
+// else {
+// 	var databaseConnectionString = dbConfigLocal;
+// }
 
-module.exports = databaseConnectionString;
-		
+// module.exports = databaseConnectionString;
