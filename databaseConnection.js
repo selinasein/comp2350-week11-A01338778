83251cab924a609233f241386b0572f7269ec778
selinasein <neulmisscj@gmail.com>
2023-03-29 18:06:03 -0700

    second commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_qoddi = process.env.IS_QODDI || false;
 const qoddiURI =
-  "mongodb+srv://spark359:accidentalLoginSteps@clusters.72edveh.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://spark359:Mselinasein7*B@clusters.72edveh.mongodb.net/?retryWrites=true&w=majority";
 const localURI =
   "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 if (is_qoddi) {
